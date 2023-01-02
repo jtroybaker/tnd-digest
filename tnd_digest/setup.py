115fa8c78126f9571e1f26d44f5fb6fa390145dc
jtroybaker <jtroybaker@live.com>
2023-01-02 00:31:58 -0500

    initial commit, basic exploration notebook

---
+++ tnd_digest/setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup, find_packages
+
+setup(name="tnd_digest",
+      version="0.1")
---
+++ tnd_digest/tnd_digest/config.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+import tnd_digest.credentials as creds
+
+# data_dir = Path('/path/to/some/logical/parent/dir')
+# data_path = data_dir / 'my_file.csv'
+
+# customer_db_url = 'sql:///customer/db/url'
+# purchases_db_url = 'sql:///purchases/db/url'
+
+user = creds.user
+password = creds.password
